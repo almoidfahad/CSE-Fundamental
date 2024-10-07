16f4891e9bc6a54c503e939ad065490b930a8690
AL Moid Fahad <almoidfahad@gmail.com>
2024-10-07 22:26:27 +0600

    Advance digit Cf problem

---
+++ C Program/Bashci/Week 2/codeforces_problem.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+
+    // problem 1
+    // char c;
+    // scanf("%d", &c);
+
+    // char start='a';
+    // char end= 'b';
+    // if(start<=end){
+    //     int next_letter=start+1;
+    //     printf("%c\n",next_letter );
+    // }
+
+
+
+    // problem 2
+    
+}
---
+++ C Program/Bashci/Week 2/tempCodeRunnerFile.c
@@ -0,0 +1,3 @@
+   else{
+        printf("Invalid Char");
+    }
