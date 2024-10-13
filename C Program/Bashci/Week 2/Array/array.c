5c9a95dd62684b288f43625bd0fedeab0e841c62
AL Moid Fahad <almoidfahad@gmail.com>
2024-10-13 06:21:13 +0600

    Sum and Average of an array Element

--- C Program/Bashci/Week 2/Array/array.c
+++ C Program/Bashci/Week 2/Array/array.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(){
-    double a[5];
+
+    // double a[5];
 
     // Access single value in Array 
 
@@ -11,12 +12,37 @@ int main(){
 
 
     //Access multiple value in Array
-    for(int i = 0; i<5; i++){
-        scanf("%lf", &a[i]);
-    }
+//     for(int i = 0; i<5; i++){
+//         scanf("%lf", &a[i]);
+//     }
     
 //    for (int i= 0; i<5;i++){
-        printf("%lf ", a[3]);
+//         printf("%lf \n", a[i]);
 //    } 
-  
+
+
+    //Sum and Average of an array Element
+
+    int n;
+    scanf("%d",&n);
+
+    int a[n];
+    for(int i = 0; i< n;i++){
+        scanf("%d", &a[i]);
+    } 
+
+    int sum = 0 ;
+    
+    for(int i= 0; i < n;i++){
+        //Every index value can do sum
+        // sum = sum+ a[i];
+        sum += a[i];
+        // printf("%d\n", sum);
+    }
+    printf("%d\n", sum);
+
+    double avg = sum / n;
+    printf("%.2lf",avg); 
+
+
 }
