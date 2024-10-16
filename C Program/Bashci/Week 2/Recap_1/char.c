5fa1fdf0bf0c0043c463f6431c9fd6966cec2ccf
AL Moid Fahad <almoidfahad@gmail.com>
2024-10-16 07:20:16 +0600

    Practic day Problem1 & Recap Module 1 Prblem 1

---
+++ C Program/Bashci/Week 2/Recap_1/char.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+int main(){
+
+
+    
+
+
+//=============================================================
+/*
+    // Problem_1 Char
+
+    Given a letter X. If the letter is lowercase print the letter after converting it from lowercase letter to uppercase letter. Otherwise print the letter after converting it from uppercase letter to lowercase letter
+
+    Note : difference between 'a' and 'A' in ASCII is 32 .
+
+    Input
+    Only one line containing a character X which will be a capital or small letter.
+
+    Output
+    Print the answer to this problem.
+
+    Examples
+    Input
+    a
+    Output
+    A
+    InputCopy
+    A
+    OutputCopy
+    a
+*/
+
+//     char ch;
+//     scanf("%c", &ch);
+
+
+//     if('a'<=ch && ch<='z'){
+//         //Apper case
+//         ch = ch - 32;
+//     }
+//     else{
+//         //Lower case
+//         ch = ch + 32;
+//     }
+//         printf("%c", ch);
+// }
--- C Program/Bashci/Week 2/practice_day2.c
+++ C Program/Bashci/Week 2/practice_day2.c
@@ -1,4 +1,25 @@
+
 #include <stdio.h>
 int main(){
-    
+
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+
+    for(int i=0; i<n; i++){
+        scanf("%d", &a[i]);
+    }
+
+    long long int sum = 0;
+
+    for(int i= 0; i<n; i++){
+            sum += a[i]; 
+    }
+    if(sum<0){
+        sum = sum * -1;
+    }
+    printf("%lld",sum);
+
+
 }
