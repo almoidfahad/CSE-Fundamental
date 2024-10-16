0c7c10c6e6feab8e02f0bd1cd44a9e66e40c4c15
AL Moid Fahad <almoidfahad@gmail.com>
2024-10-16 14:49:43 +0600

    recap problem 2

--- C Program/Bashci/Week 2/Recap_1/char.c
+++ C Program/Bashci/Week 2/Recap_1/char.c
@@ -1,9 +1,66 @@
 #include <stdio.h>
 int main(){
 
+}    
+//=========================================================================
+//problem 2
+ /*
+Given a comparison symbol S between two numbers A and B. Determine whether it is Right or Wrong.
 
-    
+The comparison is as follows: A < B, A > B, A = B.
 
+Where A, B are two integer numbers and S refers to the sign between them.
+
+Input
+Only one line containing A, S and B respectively (-100  ≤  A, B  ≤  100), S can be ('<', '>','=') without the quotes.
+
+Output
+Print "Right" if the comparison is true, "Wrong" otherwise.
+
+Examples
+InputCopy
+5 > 4
+OutputCopy
+Right
+InputCopy
+9 < 1
+OutputCopy
+Wrong
+InputCopy
+4 = 4
+OutputCopy
+Right
+    */
+//     int a, b;
+//     char s;
+//     scanf("%d %c %d",&a, &s, &b);
+
+//     if(s == '>'){
+//         if (a > b){
+//             printf("Right\n");
+//         }
+//         else{
+//             printf("Wrong\n");
+//         }
+//     }
+//     else if(s == '<'){
+//         if(a < b){
+//             printf("Right\n");
+//         }
+//         else{
+//             printf("Wrong\n");
+//         }
+//     }
+//     else{
+//         if( a == b){
+//             printf("Right\n");
+//         }
+//         else{
+//             printf("Wrong\n");
+//         }
+//     }
+//     return 0;
+// }
 
 //=============================================================
 /*
