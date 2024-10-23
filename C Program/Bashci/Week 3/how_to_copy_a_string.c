d1858386ac72c4884a7b8ea86f9ca76826af472d
AL Moid Fahad <almoidfahad@gmail.com>
2024-10-23 09:18:06 +0600

    learn function strcpy

--- C Program/Bashci/Week 3/how_to_copy_a_string.c
+++ C Program/Bashci/Week 3/how_to_copy_a_string.c
@@ -1,21 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 int main(){
-    char a[10]="mnk";
+    char a[10];
     char b[10]="abcdef";
 
 
-    int length = strlen(b); 
+    /*
+        //strings can be copied very easily through this function 
+          strcpy(a, "hello");
+        printf("%s", a);
 
-    printf("%s %s\n",a, b );
+    */
     
-    for (int i = 0; i < length; i++){
-        a[i] = b[i];
-    }
+    
 
-    a[length] = '\0';
+    //strings can be copied very easily through this function 
+    strcpy(a, b);
+     printf("%s %s", a, b);
+    
 
-    printf("%s %s", a, b);
+  /*
+      int length = strlen(b); 
+
+        printf("%s %s\n",a, b );
+        
+        for (int i = 0; i < length; i++){
+            a[i] = b[i];
+        }
+
+        a[length] = '\0';
+        
+    // printf("%s %s", a, b);
+  */
+
+   
     
     return 0;
 }
