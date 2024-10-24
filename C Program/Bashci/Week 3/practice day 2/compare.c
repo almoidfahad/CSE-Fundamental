8365c8cb854498e9d4c2d8dd7952f0f1641068a9
AL Moid Fahad <almoidfahad@gmail.com>
2024-10-24 06:54:03 +0600

    compare

---
+++ C Program/Bashci/Week 3/practice day 2/compare.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char x[21];
+    char y[21];
+
+    scanf("%s %s", x,y);
+
+    
+    if(strcmp(x,y) < 0){
+        printf("%s",x);
+    }
+
+    else{
+        printf("%s",y);
+    }
+
+}
