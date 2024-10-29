37ebeba359f41f8544ca960fed2c6b3ee96ac416
AL Moid Fahad <almoidfahad@gmail.com>
2024-10-29 23:25:41 +0600

    Frequency Arrary Problem

---
+++ C Program/Bashci/Week 4/problem_frequency_array.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+int f[100005];
+
+int main(){
+
+    int n,m;
+    scanf("%d %d", &n, &m);
+
+    int a[n + 5];
+
+    for (int  i = 0; i < n; i++){
+
+        scanf("%d", & a[i]);
+    
+        f[a[i]] += 1;
+    }
+
+    for (int i = 1; i <= m; i++)
+    {
+        printf("%d\n", f[i]);
+    }
+    
+    /*
+    int n,m;
+    scanf("%d %d", &n, &m);
+
+    int a[n + 5];
+
+    for (int  i = 0; i < n; i++){
+
+        scanf("%d", & a[i]);
+    
+        f[a[i]] += 1;
+    }
+
+    for (int i = 1; i <= m; i++)
+    {
+        printf("%d %d\n", i, f[i]);
+    }
+    */
+    
+}
