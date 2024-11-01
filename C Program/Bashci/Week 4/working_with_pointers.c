2ec9dd80507449fdcc95337090aa9313efab21c2
AL Moid Fahad <almoidfahad@gmail.com>
2024-11-01 15:42:09 +0600

    working with pointers

---
+++ C Program/Bashci/Week 4/working_with_pointers.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+    
+    int a = 5, b= 6;
+    int *x = &a, *y = &b;
+
+    printf("%d %d\n", a, b);
+    int temp =  *x;
+
+    *x = *y;
+    *y = temp;
+
+    printf("%d %d\n", a, b);
+
+
+    /*
+        int a  = 5;
+        int *b = &a;
+
+        printf("%d\n", &a);
+        printf("%d\n", b);
+        printf("%d\n", *b);
+    */
+}
