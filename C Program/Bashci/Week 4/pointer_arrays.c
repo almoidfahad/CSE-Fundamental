c15f971aac68de0053c3308f81043c0fd835213d
AL Moid Fahad <almoidfahad@gmail.com>
2024-11-03 17:08:04 +0600

    unit mattrix

---
+++ C Program/Bashci/Week 4/pointer_arrays.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+
+    int arr[5] = {1,2,3,4,5};
+
+    int *p = arr;
+
+    printf("%d %d\n", &arr[0], p);
+    printf("%d\n", (p+1));
+
+    char a = 6;
+
+    char *x = &a;
+
+    printf("%d %d", x, x+1);
+
+
+/*
+    int arr[5] = {1,2,3,4,5};
+
+    int *p = arr;
+
+    printf("%d %d\n", &arr[0], p);
+    printf("%d\n", (p+1));
+
+    int a = 6;
+
+    int *x = &a;
+
+    printf("%d %d", x, x+1);
+
+*/
+
+}
