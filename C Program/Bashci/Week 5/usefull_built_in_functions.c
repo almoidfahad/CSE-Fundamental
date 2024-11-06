8c146462570efef1c3cce53d0169591157874b41
Fahad <almoidfahad@gmail.com>
2024-11-06 11:52:23 +0600

    usefull built in function

---
+++ C Program/Bashci/Week 5/usefull_built_in_functions.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    double a = 10.49;
+
+    printf("%.2lf\n", ceil(a));
+
+    printf("%.2lf\n", floor(a));
+
+    printf("%.2lf\n", round(a));
+
+    printf("%.2lf\n", sqrt(26));
+
+    printf("%.2lf\n", pow(5,2 ));
+}
+
+/*
+//output
+
+11.00
+10.00
+10.00
+5.10
+25.00
+
+*/
