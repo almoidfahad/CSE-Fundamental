3066b83038e1359fdb5357657aba44e7bcd86af0
Fahad <almoidfahad@gmail.com>
2024-11-07 15:01:00 +0600

    Passing_array and siting in function

---
+++ C Program/Bashci/Week 5/Passing_array_and_string_in_function.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+void makeFirstCahrUpper(char x[]){
+    
+    printf("Third Function: Make First cahrecter upper case\n");
+
+    x[0] = toupper(x[0]);
+
+    printf("%s\n",x);
+}
+
+int length(char x[]){
+
+    int  n= strlen(x);
+    
+    printf("Second Function:Find length charcter strig\n");
+
+    printf("%d\n", n);
+
+
+}
+
+int fun(int x[], int n){
+
+    int sum = 0;
+
+    printf("First Function:Sum All index value\n");
+    for (int i = 0; i < n; i++){
+
+    // printf("%d ", x[i]);
+
+    sum = sum + x[i];
+        
+    }
+
+    return sum;
+}
+
+int main(){
+    int a[10] = {1, 8, 4, 3, 2, 4, 1, 5, 3, 5};
+    char c[20] = "programming";
+
+    // ( a ) is array name and ( 10 ) is array of length
+    int res = fun(a, 10);
+
+    printf("%d\n",res);
+
+    length(c);
+    makeFirstCahrUpper(c);
+}
