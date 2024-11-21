17d3e56f924351d2806173a32167f6e351c6b5ff
Fahad <almoidfahad@gmail.com>
2024-11-21 10:47:06 +0600

    Dynamic array and static array

---
+++ C++  DSA/week 1/Module 2/dynamci_array.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int * a = new int[5];
+    for (int i = 0; i < 5; i++)
+    {
+        cin>> a[i];
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        cout<< a[i] <<" ";
+    }
+    
+    
+
+    return 0;
+}
