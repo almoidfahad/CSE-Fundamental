cdc1770248edec041271043b2e16aa5342b2ce06
Fahad <almoidfahad@gmail.com>
2024-11-26 10:40:02 +0600

    dynamic allocation of array cpp

--- C++  DSA/week 1/Module 2/pointer_of_array.cpp
+++ C++  DSA/week 1/Module 2/pointer_of_array.cpp
@@ -1,6 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
+    int arr [5];
+
+
+    *(arr) = 10;
+
+    *(arr+1) = 20;
+
+    *(arr+2)= 30;
+
+    *(arr + 3) = 40;
+
+    *(arr + 4) = 50;
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout<< (arr + i)<<endl;
+    }
     
+
+
+
+
+
+    // cout<< &arr[0] <<endl; //arr er prothom ghor er address
+
+    // cout<<arr <<endl; //arr namm tar maan 
+
+
+
+
     return 0;
 }
