e701402542e052507023fcf49ac0e332413fcc7e
Fahad <almoidfahad@gmail.com>
2024-11-27 07:02:28 +0600

    update class and object

--- C++  DSA/week 1/Module 3/class_and_object.cpp
+++ C++  DSA/week 1/Module 3/class_and_object.cpp
@@ -17,10 +17,16 @@ int main(){
     // int a;
     Student a;
 
-    a.roll =  20;
-    a.gpa = 4.5;
-    char temp[100] = "Sakib";
-    strcpy(a.name, temp);
+    // a.roll =  20;
+    // a.gpa = 4.5;
+    // char temp[100] = "Sakib";
+    // strcpy(a.name, temp);
+    
+    cin.getline(a.name, 100);
+
+    cin.ignore();
+
+    cin>>a.roll >> a.gpa ;
 
     cout<< a.name <<" "<<a.roll << " " <<a.gpa <<endl;
 
