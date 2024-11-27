4b1c96c26149241b6a37c9a89dc4253c3bddd4b3
Fahad <almoidfahad@gmail.com>
2024-11-27 09:04:11 +0600

    Constructor

---
+++ C++  DSA/week 1/Module 3/constructor.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+    public:
+    int roll;
+    int cls;
+    double gpa; 
+
+    // Student(int r, int c, double g){
+    //     roll = r;
+    //     cls = c;
+    //     gpa = g;
+    // }
+
+
+
+};
+
+
+int main(){
+
+    Student rahim;
+    cin>>rahim.roll>>rahim.cls>>rahim.gpa; 
+
+
+//================================================
+    // int r;
+    // int c;
+    // double g;
+    // cin>> r >> c >> g;
+
+    // Student rahim(r, c , g);
+
+    // Student karim(2, 5, 5.00);
+
+//==============================================
+    // Student rahim;
+    // rahim.roll = 45;
+    // rahim.cls = 5;
+    // rahim.gpa = 3.20;
+
+    // Student karim;
+    // karim.roll = 2;
+    // karim.cls = 5;
+    // karim.gpa = 5.00;
+
+
+    cout<<rahim.roll <<" " << rahim.cls << " " << rahim.gpa <<endl;
+    // cout<<karim.roll <<" " << karim.cls << " " << karim.gpa <<endl;
+
+
+    return 0;
+}
